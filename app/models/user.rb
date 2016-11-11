e30ecbbc9256406aa0c66dc337b26e5a65575e20
nothingisfunny <if.the.kids.r.united@gmail.com>
2016-11-10 19:43:56 -0800

    add role enum and default role to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,17 @@
 class User < ActiveRecord::Base
   has_many :user_festival
   has_many :festival, through: :user_festival
+  enum role: [:user, :admin]
+  after_initialize :set_default_role, :if => :new_record?
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :omniauthable, :omniauth_providers => [:facebook, :google_oauth2]
-
+ 
+  def set_default_role
+    self.role ||= :user
+  end
+  
  def self.from_omniauth(auth)
     where(provider: auth[:provider], uid: auth.uid).first_or_create do |user|
       user.email = auth[:info][:email]
