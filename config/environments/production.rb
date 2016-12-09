bb401a1142253e5549c0c3c3eaf2c7d32d0e247c
nothingisfunny <if.the.kids.r.united@gmail.com>
2016-12-09 18:13:53 +0300

    move better errors into dev initializer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,5 +76,4 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  BetterErrors::Middleware.allow_ip! ENV['TRUSTED_IP'] if ENV['TRUSTED_IP']
-end
+  end
