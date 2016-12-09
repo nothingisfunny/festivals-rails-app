eef50eb8d41a75580b54d1732acce588a8fae298
nothingisfunny <if.the.kids.r.united@gmail.com>
2016-12-09 17:55:26 +0300

    handle unique email error with omniauth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,15 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
+  rescue_from PG::UniqueViolation, with: :email_already_registered
   include Pundit
   protect_from_forgery with: :exception
   def after_sign_in_path_for(resource)
   	request.env['omniauth.origin'] || root_path
+  end
+
+  def email_already_registered
+  	flash[:error] = "Sorry. This email was already registered. Try the sign in button."
+  	redirect_to '/'
   end
 end
