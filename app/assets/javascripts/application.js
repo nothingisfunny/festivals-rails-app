3c6c9643da083f77ef7f223e85028524a57b84be
nothingisfunny <if.the.kids.r.united@gmail.com>
2017-04-11 10:25:25 -0700

    save

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -30,3 +30,34 @@ function filterFestivals(){
 // GRAB FILTER VALUES
 //$('[name="category"] option:selected').text()
 //$('[name="world_part"] option:selected').text()
+
+$(function showComments(){
+	$("a.show_comments").on("click", function(e){
+		$.get(this.href).success(function(json){
+			console.log(json)
+			$("#comments").append('<h2>' + json.length + ' Comment(s):</h2>')
+			json.forEach(function(comment){
+				$("#comments").append('<p>' + '<strong>' + comment.username + ' said: </strong>' + comment.content + '</p>');
+			})
+		})
+		$.get(this.href + '/new').success(function(response){
+			$("#new-comment-form").append(response);
+		})
+		e.preventDefault();
+	})
+})
+
+$(function appendComment(){
+	$("#new_comment").on("submit", function(event){
+		$.ajax({
+			type: this.method, 
+			url: this.action,
+			data: $(this).serialize(),
+			success: function(response){
+				$("#comments").append(response);
+				$("#comment_content").val('');
+			}
+		})
+		event.preventDefault();
+	})
+})
