d151b3ec204b448aff297905d6a01ddaec3c8fef
nothingisfunny <if.the.kids.r.united@gmail.com>
2018-02-25 17:08:15 -0800

    fix the 301 redirect error when fetching festival data

--- lib/festival_fetcher.rb
+++ lib/festival_fetcher.rb
@@ -1,19 +1,23 @@
-require 'net/https'
+require 'net/http'
 
 class FestivalFetcher
   def self.fetch
       url = URI.parse(Rails.application.config.predicthq_url)
       url.query = URI.encode_www_form("query" => "festival")
-      headers = {
-      'Authorization'=>"Bearer: #{ENV["PREDICTHQ_SECRET"]}",
-      'Content-Type' =>'application/json',
-      'Accept'=>'application/json'
-        }
+      
       req = Net::HTTP::Get.new(url.to_s)
-      https = Net::HTTP.new(url.host, url.port, headers )
-      https.use_ssl = true
+      req.initialize_http_header({'Authorization'=>"Bearer #{ENV["PREDICTHQ_SECRET"]}"})
+      https = Net::HTTP.start(url.host, url.port, :use_ssl => true)
+      
       res = https.request(req)
-      puts res
+      if res.code == "301"
+        url = URI.parse(res.header['location'])
+        url.query = URI.encode_www_form("query" => "festival")
+        req = Net::HTTP::Get.new(url.to_s)
+        req.initialize_http_header({'Authorization'=>"Bearer #{ENV["PREDICTHQ_SECRET"]}"})
+        res = https.request(req)
+      end
+      res
       # http = Net::HTTP.new(url.host, url.port)
       # http.use_ssl  = true
       
