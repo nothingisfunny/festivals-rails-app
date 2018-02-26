d2c233c8f1b8d0f4a9e02750b8950786432306e3
nothingisfunny <if.the.kids.r.united@gmail.com>
2018-02-25 21:54:49 -0800

    change query to q to fetch festivals correctly

--- lib/festival_fetcher.rb
+++ lib/festival_fetcher.rb
@@ -3,7 +3,7 @@ require 'net/http'
 class FestivalFetcher
   def self.fetch
       url = URI.parse(Rails.application.config.predicthq_url)
-      url.query = URI.encode_www_form("query" => "festival")
+      url.query = URI.encode_www_form("q" => "festival")
       
       req = Net::HTTP::Get.new(url.to_s)
       req.initialize_http_header({'Authorization'=>"Bearer #{ENV["PREDICTHQ_SECRET"]}"})
@@ -12,16 +12,11 @@ class FestivalFetcher
       res = https.request(req)
       if res.code == "301"
         url = URI.parse(res.header['location'])
-        url.query = URI.encode_www_form("query" => "festival")
+        url.query = URI.encode_www_form("q" => "festival")
         req = Net::HTTP::Get.new(url.to_s)
         req.initialize_http_header({'Authorization'=>"Bearer #{ENV["PREDICTHQ_SECRET"]}"})
         res = https.request(req)
       end
-      res
-      # http = Net::HTTP.new(url.host, url.port)
-      # http.use_ssl  = true
-      
-      # resp = http.get(req, headers)
-      # puts resp
+      return JSON.parse(res.body)["results"]
   end
 end
